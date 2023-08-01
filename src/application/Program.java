0dd0f4112caf6c3dd1a75314d2d56cc644d42696
Alexandre Ferreira <alexandrejfneto@gmail.com>
2023-08-01 14:20:41 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,48 +12,58 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println("*** TEST 1: Seller findById ***");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		
+
 		System.out.println();
-		
+
 		System.out.println("*** TEST 2: Seller findByDepartment ***");
 		Department dep = new Department(2, null);
-		List <Seller> listSeller = sellerDao.findByDepartment(dep);
-		//listSeller.sort((sell1, sell2) -> sell2.getName().toUpperCase().compareTo(sell1.getName().toUpperCase()));
+		List<Seller> listSeller = sellerDao.findByDepartment(dep);
+		// listSeller.sort((sell1, sell2) ->
+		// sell2.getName().toUpperCase().compareTo(sell1.getName().toUpperCase()));
 		for (Seller sel : listSeller) {
 			System.out.println(sel);
 		}
-		
+
 		System.out.println();
-		
+
 		System.out.println("*** TEST 3: Seller findAll ***");
-		List <Seller> listSeller2 = sellerDao.findAll();
+		List<Seller> listSeller2 = sellerDao.findAll();
 		for (Seller sel : listSeller2) {
 			System.out.println(sel);
 		}
 
 		System.out.println();
-		
+
 		/*
-		System.out.println("*** TEST 4: Seller insert ***");
-		Seller newSeller = new Seller(null, "Greg", "greg@gamil.com", new Date(), 4000.00, dep);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New Id = " + newSeller.getId());
+		System.out.println("*** TEST 4: Seller insert ***"); Seller newSeller = new
+		Seller(null, "Greg", "greg@gamil.com", new Date(), 4000.00, dep);
+		sellerDao.insert(newSeller); System.out.println("Inserted! New Id = " +
+		newSeller.getId());
 		*/
 
 		System.out.println();
-		
+
 		System.out.println("*** TEST 5: Seller update ***");
 		seller = sellerDao.findById(1);
 		System.out.println(seller.getName());
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update complete! New name = " + seller.getName());
-		
+
+		System.out.println();
+
+		System.out.println("*** TEST 6: Seller delete ***");
+		System.out.print("Enter id for delete test: ");
+		Scanner sc = new Scanner(System.in);
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete complete!");
+		sc.close();
 	}
 
 }
